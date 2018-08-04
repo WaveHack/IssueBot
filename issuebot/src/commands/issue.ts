a05364d02186a54acafce3aed9ea3bf46ea68f7a
WaveHack <email@wavehack.net>
2018-08-05 00:54:49 +0200

    Refactor !issue command

--- issuebot/src/commands/issue.ts
+++ issuebot/src/commands/issue.ts
@@ -1,56 +1,76 @@
 import Bot from '../issuebot'
-import { Command }  from 'discord-harmony'
-const config = require('../../config.json')
+import {Command} from 'discord-harmony'
+
+const config = require('../../config.json');
 
 export class IssueCommand extends Command {
-  execute() {
-    if(!this.args) {
-      return
-    }
-    let issueBody = ISSUE_TEMPLATE
-      .replace("{PLACEHOLDER}", this.args[2] || "_No content_")
-      .replace("{CHANNEL}", this.message.channel.name)
-      .replace("{USER}", this.message.author.username)
-    Bot.gitHub.api.issues.create({
-        owner: config.name,
-        repo: this.args[0],
-        title: this.args[1],
-        body: <any>issueBody // Typings for the `github` package are incorrect, so we have to cast to any here.
-      },
-      (error, response) => this.handleGithubResponse(error, response)
-    )
-  }
 
-  handleGithubResponse(error, response) {
-    if(error) {
-      let formattedError = JSON.stringify(error, null, 4)
-      let reply = ERROR_TEMPLATE.replace('{PLACEHOLDER}', formattedError)
-      this.message.reply(reply)
-      return
+    execute(): void {
+        if (!this.args) {
+            return
+        }
+
+        // if defaultRepo
+        // get repository from config
+        // else
+        const repository: string = this.args.shift();
+
+        const title: string = this.args.shift();
+        const body: string = this.args.shift();
+
+        if (!repository || !title) {
+            return;
+        }
+
+        let issueBody = ISSUE_TEMPLATE
+            .replace("{ISSUE_BODY}", (body || '_No content_'))
+            .replace("{CHANNEL}", this.message.channel.name)
+            .replace("{USER}", this.message.author.username);
+
+        Bot.gitHub.api.issues.create({
+            owner: config.name,
+            repo: repository,
+            title: title,
+            body: <any>issueBody // Typings for the `github` package are incorrect, so we have to cast to any here.
+        }, (error, response) => this.handleGithubResponse(error, response));
     }
-    let reply = SUCCESS_TEMPLATE.replace('{PLACEHOLDER}', response.html_url)
-    this.message.reply(reply)
-  }
+
+    handleGithubResponse(error, response) {
+        if (error) {
+            const reply = ERROR_TEMPLATE
+                .replace('{CODE}', error.code.toString())
+                .replace('{STATUS}', error.status)
+                .replace('{MESSAGE}', JSON.stringify(JSON.parse(error.message), null, 2));
+
+            this.message.reply(reply);
+
+            return;
+        }
+
+        const reply = SUCCESS_TEMPLATE
+            .replace('{URL}', response.html_url);
+
+        this.message.reply(reply)
+    }
+
 }
 
-const ISSUE_TEMPLATE =
-`
-{PLACEHOLDER}
+const ISSUE_TEMPLATE = `
+{ISSUE_BODY}
 
 ---
 Beep, boop, I'm [a bot](https://github.com/LeagueSandbox/IssueBot)! This issue was created by \`@{USER}\` in \`#{CHANNEL}\`.
-`
+`;
 
-const ERROR_TEMPLATE =
-`
-An error occurred while creating the issue.
-Details:
-\`\`\`
-{PLACEHOLDER}
-\`\`\`
-`
+const ERROR_TEMPLATE = `
+An error has occurred while creating the issue: **{CODE}: {STATUS}**.
 
-const SUCCESS_TEMPLATE =
-`
-Issue created successfully! {PLACEHOLDER}
-`
+Message:
+\`\`\`json
+{MESSAGE}
+\`\`\`
+`;
+
+const SUCCESS_TEMPLATE = `
+Issue created successfully! {URL}
+`;
