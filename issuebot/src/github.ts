735c8ad2f269ce859b83da928b3acfa8eae11178
WaveHack <email@wavehack.net>
2018-08-05 13:09:53 +0200

    Update github.ts
    Add semicolon and remove unused method

--- issuebot/src/github.ts
+++ issuebot/src/github.ts
@@ -9,16 +9,7 @@ export class GitHub {
         this.api.authenticate({
             type: 'oauth',
             token: token
-        })
+        });
     }
-
-    // submitIssue(owner, repo, title, description) {
-    //     return this.api.issues.create({
-    //         owner: owner,
-    //         repo: repo,
-    //         title: title,
-    //         body: description
-    //     });
-    // }
 
 }
